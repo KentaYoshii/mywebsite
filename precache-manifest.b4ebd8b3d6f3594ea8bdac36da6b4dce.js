4d06c57624ab69501a4c487fd1ee2e5ec6dd0ff8
KentaYoshii <kenta_yoshii@brown.edu>
2021-12-23 21:03:54 -0500

    Updates

---
+++ precache-manifest.b4ebd8b3d6f3594ea8bdac36da6b4dce.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "49e73ba4b4137d5c70df78a4cc0700e6",
+    "url": "/mywebsite/index.html"
+  },
+  {
+    "revision": "c4d4bad5af78266429fe",
+    "url": "/mywebsite/static/css/2.c5c7f9a8.chunk.css"
+  },
+  {
+    "revision": "3a297173e94a1fe380c0",
+    "url": "/mywebsite/static/css/main.8052061f.chunk.css"
+  },
+  {
+    "revision": "c4d4bad5af78266429fe",
+    "url": "/mywebsite/static/js/2.cc035686.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/mywebsite/static/js/2.cc035686.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3a297173e94a1fe380c0",
+    "url": "/mywebsite/static/js/main.86f3cb33.chunk.js"
+  },
+  {
+    "revision": "8bfdc81bc68db2f39e17",
+    "url": "/mywebsite/static/js/runtime-main.2e72d8b5.js"
+  },
+  {
+    "revision": "ce7834d1f203c4d299d70fe5d0f85319",
+    "url": "/mywebsite/static/media/DemonAttack_max.ce7834d1.gif"
+  },
+  {
+    "revision": "a756e0112873e52212b583f157860a26",
+    "url": "/mywebsite/static/media/developer.a756e011.jpeg"
+  },
+  {
+    "revision": "33235f8db0dc9baea6d5206139c98738",
+    "url": "/mywebsite/static/media/plantLookup2.33235f8d.png"
+  },
+  {
+    "revision": "e4b33ff06fcbaf536730a5f0a95b5889",
+    "url": "/mywebsite/static/media/portfolio.e4b33ff0.png"
+  }
+]);
